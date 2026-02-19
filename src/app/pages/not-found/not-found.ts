73fe215d1424fc5ad5cc1dc48859708beeb3b234
Renato Lins <renato.lins.digital@outlook.com>
2026-02-19 19:36:44 -0300

    improves the not found page

--- src/app/pages/not-found/not-found.ts
+++ src/app/pages/not-found/not-found.ts
@@ -1,6 +1,5 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { ResponsiveService } from '../../domain/services/responsive.service';
 import { PageContainerComponent } from '../../domain/components/page-container/page-container';
 import { AppButtonComponent } from '../../shared/components/button/button';
 
@@ -12,7 +11,6 @@ import { AppButtonComponent } from '../../shared/components/button/button';
 })
 export class NotFoundComponent {
   protected readonly router = inject(Router);
-  protected readonly responsive = inject(ResponsiveService);
 
   protected goToHome(): void {
     this.router.navigate(['/store']);
